a2d664c7d8d4f30be65018c9e42f27e6d16d5d42
Venkatesh200510 <venkateshsgounskar@gmail.com>
2025-10-30 20:22:11 +0530

    .env

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise");
 
 // Create a MySQL connection pool
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME,
+  port: process.env.DBPORT||3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
