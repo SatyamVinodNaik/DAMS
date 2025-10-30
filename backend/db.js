3c80cc7bfb95bd92cbaf8e9909bdc48454a23ec3
Venkatesh200510 <venkateshsgounskar@gmail.com>
2025-10-30 22:20:31 +0530

    .env

--- backend/db.js
+++ backend/db.js
@@ -6,10 +6,18 @@ const db = mysql.createPool({
   user: process.env.DBUSER,
   password: process.env.DBPASSWORD,
   database: process.env.DBNAME,
-  port: process.env.DBPORT||3306,
+  port: process.env.DBPORT||19335,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
+});
+
+const sessionStore = new MySQLStore({
+  host: process.env.DB_HOST || "ballast.proxy.rlwy.net",
+  port: process.env.DB_PORT || 19335,
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "KJAmtjCVlJOYYmONvoMnGPGvqTnijJPM",
+  database: process.env.DB_NAME || "railway",
 });
 
 // ✅ Test connection on startup
